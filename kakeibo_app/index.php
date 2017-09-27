6b6b2fb2000fa45cd07a28b29921c3c5c1d36da0
Seiji Shii <seiji.shii@gmail.com>
2017-09-27 19:18:29 +0900

    Initial commit.

---
+++ kakeibo_app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>家計簿アプリ</title>
+    </head>
+    <body>
+        <h1>家計簿アプリ</h1>
+        <a href="">ログイン</a>
+        <ul>
+            <li>予算</li>
+            <li>収支</li>
+            <li>集計</li>
+        </ul>
+    </body>
+</html>
